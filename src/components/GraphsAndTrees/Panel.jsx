bcd2f0741390e8afeb8ac537b2cbae1c6d0a7898
Joraeuw <joraeuw@gmail.com>
2021-10-02 21:50:44 +0300

    Working bezier curve 🎆

--- src/components/GraphsAndTrees/Panel.jsx
+++ src/components/GraphsAndTrees/Panel.jsx
@@ -1,5 +1,10 @@
+import Bezier from './node/BezierCurves';
 const Panel = () => {
-  return <div className="panelDiv">Hi</div>;
+  return (
+    <div className="panelDiv">
+      <Bezier viewBoxWidth="1800" viewBoxHeight="1800" />
+    </div>
+  );
 };
 
 export default Panel;
