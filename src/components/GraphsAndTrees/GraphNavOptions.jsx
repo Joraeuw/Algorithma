e1308da85f1ea2e4eedd6010effd7a5b958d2243
Joraeuw <joraeuw@gmail.com>
2021-11-03 17:54:35 +0200

    Implemented DFS for binary tree 🌳🌳🌳

--- src/components/GraphsAndTrees/GraphNavOptions.jsx
+++ src/components/GraphsAndTrees/GraphNavOptions.jsx
@@ -1,6 +1,21 @@
 import Node from './node/Node';
+import DFSByValue from './Trees/BinaryTreeDFS';
+
+const onButtonClick = () => {
+  //SHOULD MAKE A SELECTOR FOR THIS
+  const value = 'node0';
+  const path = DFSByValue(value);
+  console.log(path);
+};
+
 const GraphNavOptions = () => {
-  return <div></div>;
+  return (
+    <div>
+      <button className="bg-pink-300 rounded-md" onClick={onButtonClick}>
+        Calculate
+      </button>
+    </div>
+  );
 };
 
 export default GraphNavOptions;
